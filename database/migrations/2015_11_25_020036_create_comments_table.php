ada7751e7ff9a3748397819ce19c6e21acb7850b
Pramesti Hatta K <opam.qweiop@gmail.com>
2015-11-25 09:23:43 +0700

    v0.0.0

---
+++ database/migrations/2015_11_25_020036_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('story_id')->unsigned();
+            $table->foreign('story_id')->references('id')->on('stories');
+            $table->string('name');
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
